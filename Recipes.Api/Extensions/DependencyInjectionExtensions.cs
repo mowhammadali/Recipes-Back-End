34a6958d51b07889e0cca2a834bcd76364966e71
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-24 12:10:00 +0330

    add dependency injection extensions

---
+++ Recipes.Api/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+}
