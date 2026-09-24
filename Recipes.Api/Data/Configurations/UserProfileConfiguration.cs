678ce0c55547851171cb21f8f5d5902b8ba56806
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-24 19:35:44 +0330

    implement user profile entity configuration

---
+++ Recipes.Api/Data/Configurations/UserProfileConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Recipes.Api.Models.Entities;
+
+namespace Recipes.Api.Data.Configurations;
+
+public class UserProfileConfiguration : IEntityTypeConfiguration<UserProfile>
+{
+    public void Configure(EntityTypeBuilder<UserProfile> builder)
+    {
+        builder.ToTable("UserProfiles");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(u => u.UserId).IsRequired();
+
+        builder.Property(u => u.FirstName).HasMaxLength(50);
+
+        builder.Property(u => u.LastName).HasMaxLength(50);
+
+        builder.Property(u => u.Bio).HasMaxLength(100);
+
+        builder.HasIndex(u => u.UserId).IsUnique();
+    }
+}
