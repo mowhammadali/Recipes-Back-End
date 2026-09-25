468e960823271a50f6fcd326978e7b721ed40ce0
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-25 17:40:36 +0330

    add user role migration

---
+++ Recipes.Api/Migrations/20260925135619_AddUserRole.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Recipes.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Difficulty",
+                table: "Recipes",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Difficulty",
+                table: "Recipes",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
