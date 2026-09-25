feb2ed5b5ea3a4f4e5b6934453ec20a1888e694f
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-25 12:33:11 +0330

    use ICollection instead of list in entities

--- Recipes.Api/Models/Entities/MealType.cs
+++ Recipes.Api/Models/Entities/MealType.cs
@@ -4,5 +4,5 @@ public sealed class MealType
 {
     public Guid Id { get; init; }
     public string Name { get; init; } = null!;
-    public List<Recipe> Recipes { get; init; } = [];
+    public ICollection<Recipe> Recipes { get; init; } = [];
 }
