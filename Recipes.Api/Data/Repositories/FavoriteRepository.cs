6d5b39b02f67849a9b7d88ca169ed2d3ca767405
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-25 12:53:41 +0330

    implement favorite repository

---
+++ Recipes.Api/Data/Repositories/FavoriteRepository.cs
@@ -0,0 +1,34 @@
+﻿using Recipes.Api.Data.Repositories.Interfaces;
+using Recipes.Api.Models.Entities;
+
+namespace Recipes.Api.Data.Repositories;
+
+public sealed class FavoriteRepository : IFavoriteRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public FavoriteRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task AddAsync(Favorite favorite)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Favorite?> GetByIdAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Favorite>> GetAllByUserIdAsync(Guid userId)
+    {
+        throw new NotImplementedException();
+    }
+}
