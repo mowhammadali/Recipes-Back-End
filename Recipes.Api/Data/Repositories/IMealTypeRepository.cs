1b8f60d4666f09abfa14d336803b78f3eaed4191
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-25 12:11:01 +0330

    implement mealtype repository

---
+++ Recipes.Api/Data/Repositories/IMealTypeRepository.cs
@@ -0,0 +1,8 @@
+﻿using Recipes.Api.Models.Entities;
+
+namespace Recipes.Api.Data.Repositories;
+
+public interface IMealTypeRepository
+{
+    Task<List<MealType>> GetAllAsync();
+}
---
+++ Recipes.Api/Data/Repositories/MealTypeRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Recipes.Api.Models.Entities;
+
+namespace Recipes.Api.Data.Repositories;
+
+public class MealTypeRepository : IMealTypeRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public MealTypeRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<List<MealType>> GetAllAsync()
+    {
+        var mealTypes = await _dbContext.MealTypes.ToListAsync();
+
+        return mealTypes;
+    }
+}
