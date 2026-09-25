b658c02081e53337294bfeff7834f5ba59ee019d
mohammad ali moradkhani <moradkhani.mohammadali@gmail.com>
2026-09-25 13:46:53 +0330

    use mapping in meal type service

--- Recipes.Api/Services/MealTypeService.cs
+++ Recipes.Api/Services/MealTypeService.cs
@@ -1,4 +1,5 @@
-﻿using Recipes.Api.Data.Repositories.Interfaces;
+﻿using AutoMapper;
+using Recipes.Api.Data.Repositories.Interfaces;
 using Recipes.Api.Models.DTOs.MealTypes;
 using Recipes.Api.Services.Interfaces;
 
@@ -7,14 +8,20 @@ namespace Recipes.Api.Services;
 public class MealTypeService : IMealTypeService
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
 
-    public MealTypeService(IUnitOfWork unitOfWork)
+    public MealTypeService(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
 
     public async Task<List<MealTypeResponse>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        var mealTypes = await _unitOfWork.MealTypes.GetAllAsync();
+
+        var response = _mapper.Map<List<MealTypeResponse>>(mealTypes);
+
+        return response;
     }
 }
